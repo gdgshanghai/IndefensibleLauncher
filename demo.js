89deca724145bd18cf1fd53018506ac2c1196258
ThrowDice <areyoulookon@gmail.com>
2014-03-01 00:50:04 +0800

    Load google home page in XMLHttpRequest.
    For xiaoma.

--- demo.js
+++ demo.js
@@ -1,7 +1,8 @@
 //alert(1);
 
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.getpostman.com/collections/c594ef706d94e390f5c3", true);
+//xhr.open("GET", "https://www.getpostman.com/collections/c594ef706d94e390f5c3", true);
+xhr.open("GET", "http://www.google.com/", true);
 xhr.onreadystatechange = function() {
   if (xhr.readyState == 4) {
     // WARNING! Might be injecting a malicious script!
