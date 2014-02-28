b96f0d8b99438970e2181bd9ac620b7a842faf57
ThrowDice <areyoulookon@gmail.com>
2014-03-01 00:23:20 +0800

    Demo cross domain http request.

---
+++ demo.js
@@ -0,0 +1,11 @@
+//alert(1);
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.getpostman.com/collections/c594ef706d94e390f5c3", true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    // WARNING! Might be injecting a malicious script!
+    document.getElementById("resp").innerHTML = xhr.responseText;
+  }
+}
+xhr.send();
