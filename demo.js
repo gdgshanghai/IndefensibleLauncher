4239a623ab19322158b31e30f88424f1bc0725a1
sorcerer-ma <sorcerer.ma@gmail.com>
2014-03-01 10:39:57 +0800

    Add Jquery.

--- demo.js
+++ demo.js
@@ -1,12 +1,14 @@
-//alert(1);
+// var xhr = new XMLHttpRequest();
+// //xhr.open("GET", "https://www.getpostman.com/collections/c594ef706d94e390f5c3", true);
+// xhr.open("GET", "http://portal.qiniu.com/", true);
+// xhr.onreadystatechange = function() {
+//     if (xhr.readyState == 4) {
+//         // WARNING! Might be injecting a malicious script!
+//         document.getElementById("resp").innerHTML = xhr.responseText;
+//     }
+// }
+// xhr.send();
 
-var xhr = new XMLHttpRequest();
-//xhr.open("GET", "https://www.getpostman.com/collections/c594ef706d94e390f5c3", true);
-xhr.open("GET", "http://www.google.com/", true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    // WARNING! Might be injecting a malicious script!
-    document.getElementById("resp").innerHTML = xhr.responseText;
-  }
-}
-xhr.send();
+$(function() {
+    alert('jquery ok');
+});
