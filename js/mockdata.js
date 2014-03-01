d45a510a411bbd3ecef4ba29524e7f2c8e8c33b1
ThrowDice <areyoulookon@gmail.com>
2014-03-01 14:13:15 +0800

    Move mock data to separate file.

---
+++ js/mockdata.js
@@ -0,0 +1,13 @@
+var mockdata = {};
+mockdata.dianping = {
+    veneus: [{
+        title: 'title1',
+        url: 'http://www.google.com'
+    }, {
+        title: 'title2',
+        url: 'http://www.baidu.com'
+    }, {
+        title: 'title3',
+        url: 'http://www.bing.com'
+    }]
+};
