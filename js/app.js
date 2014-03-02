e66e244a31b428f3ed42e184f63876b0d557a9de
ThrowDice <areyoulookon@gmail.com>
2014-03-02 12:19:53 +0800

    Add arrow.

--- js/app.js
+++ js/app.js
@@ -188,7 +188,8 @@ $(function() {
                 var label = '<br><span class="icon-label">' + v[i].replace(/\.com|\.cn|\.io|\.org|\.hk|\.jp|\.en/g, '') + '</span>';
                 a += ('<a class="icon-wrapper" target="_blank" href="http://' + v[i] + '">' + icon + label + '</a>');
             }
-            ulStr += ('<li><span class="initial categorized-initial"><img src="../images/' + k + 'logo.png"></span>' + a + '</li>');
+            arrow_div = '<div class="arrow"></div>';
+            ulStr += ('<li><span class="initial categorized-initial"><img src="../images/'+k+'logo.png"></span>' + a +arrow_div+ '</li>');
         });
         ulStr = '<ul>' + ulStr + '</ul>';
         $('#launcher-2 .left-block').html(ulStr);
